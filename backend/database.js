53e23f6ccf4118390e94dc845af1e126287f2b6a
lokeshkumar0001 <lokeshkrs2001@gmail.com>
2025-05-01 15:18:16 +0530

    add mongoose flag

--- backend/database.js
+++ backend/database.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
 
-const connectDatabase = ()=>{
-    mongoose.connect(process.env.DB_URI)
-    .then((data)=>{
-        console.log(`Mongodb connected with server: ${data.connection.host}`);
-    })
+const connectDatabase = () => {
+  mongoose.set("strictQuery", false);
+  mongoose.connect(process.env.DB_URI).then((data) => {
+    console.log(`Mongodb connected with server: ${data.connection.host}`);
+  });
+};
 
-}
-
-module.exports = connectDatabase
+module.exports = connectDatabase;
